e7e9777c6f1f424605dd2211e660e69137874a14
Vinit Shahdeo <vinitshahdeo@gmail.com>
2017-03-01 18:28:31 +0530

    score script

---
+++ script.js
@@ -0,0 +1,46 @@
+/* Author - Vinit Shahdeo */
+/* Copyright - CodeChef VIT Chapter */
+
+function scoreTeamBlue(val) {
+    var score = document.getElementById('blue').value;
+    var new_score = parseInt(score,10) + val;
+    
+    if (new_score < 0) {
+        new_score = 0;
+    }
+    
+    var path='audio/'+val+'.mp3';
+    var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', path);
+  audioElement.play();
+   document.getElementById('blue').value = new_score;
+    return new_score;
+    
+}
+
+function scoreTeamRed(val) {
+    var score = document.getElementById('red').value;
+    var new_score = parseInt(score,10) + val;
+    
+    if (new_score < 0) {
+        new_score = 0;
+    }
+    
+    var path='audio/'+val+'.mp3';
+    var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', path);
+  audioElement.play();
+   document.getElementById('red').value = new_score;
+    return new_score;
+}
+
+function reset(val){
+    var path='audio/'+val+'.mp3';
+    var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', path);
+  audioElement.play();
+    document.getElementById('red').value=0;
+    document.getElementById('blue').value=0;   
+}
+
+/* Made by Vinit Shahdeo for TechBlaze Event */
